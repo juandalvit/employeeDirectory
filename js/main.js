ddd2cf0b8db36bbdb2592fda2ccb3c0323c6e9c9
juandalvit <juan.dalvit@bits0.com>
2015-02-23 18:31:45 -0300

    alert2

--- js/main.js
+++ js/main.js
@@ -12,23 +12,27 @@ var app = {
             }
         });
     },
+    
+    showAlert: function (message, title) {
+	    if (navigator.notification) {
+    	    navigator.notification.alert(message, null, title, 'OK');
+	    } else {
+        	alert(title ? (title + ": " + message) : message);
+    	}
+	},
 
     initialize: function() {
-    	var self = this;
-	    this.store = new MemoryStore(function() {
-    	    self.showAlert('Store Initialized', 'Info');
-	    });
-    	$('.search-key').on('keyup', $.proxy(this.findByName, this));
-}
+        var self = this;
+	    this.store = new WebSqlStore(function() {
+        	self.showAlert('Store Initialized', 'Info');
+    	});
+	    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    }
+    
 
 };
 
-showAlert: function (message, title) {
-    if (navigator.notification) {
-        navigator.notification.alert(message, null, title, 'OK');
-    } else {
-        alert(title ? (title + ": " + message) : message);
-    }
-},
+
 
 app.initialize();
+
